6aeea06ef033ea7b36800df1edee69c7c8d27319
seoyounsoo <iulove37@naver.com>
2020-12-04 00:00:40 +0900

    PCC Lv.1

---
+++ lv1자연수뒤집어배열로만들기.py
@@ -0,0 +1,17 @@
+# 자연수 뒤집어 배열로 만들기
+
+def solution(n):
+    answer = []
+    a = n
+
+    while (a > 0):
+        b = a % 10
+        a = a // 10
+        answer.append(b)
+
+    return answer
+
+
+# testcase 1
+n = 404400123
+print(solution(n))
