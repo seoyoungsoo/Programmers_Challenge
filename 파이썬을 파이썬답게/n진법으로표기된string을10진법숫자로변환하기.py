3c229cafa1bbd0d25bb4efe978904250e4918af6
seoyounsoo <iulove37@naver.com>
2021-01-19 21:37:28 +0900

    파이썬을 파이썬답게

---
+++ 파이썬을 파이썬답게/n진법으로표기된string을10진법숫자로변환하기.py
@@ -0,0 +1,11 @@
+# n진법으로 표기된 string을 10진법 숫자로 변환하기
+
+num, base = map(int, input().strip().split(' '))
+n = str(num)
+l = len(n) - 1
+
+res = 0
+for i in n:
+    res += int(i) * (base ** l)
+    l -= 1
+print(res)
